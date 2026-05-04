b101d348d6750f8871a92415747ca19d3fdfcfde
MatiasAtria <matiasatria@gmail.com>
2026-05-03 22:56:51 -0400

    Fix in notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,15 @@
 class Notification < ApplicationRecord
   belongs_to :user
   validates :title, :message, presence: true
+
+  enum :notification_type {
+    registration: 'registration',
+    waitlist: 'waitlist',
+    reminder: 'reminder',
+    event: 'event',
+    cancellation: 'cancellation',
+    review: 'review'
+  }
+
+  validates :title, :message, :notification_type, presence: true
 end
